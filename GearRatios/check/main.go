8893570344ecf77edc6f83668ff7d3c256bb3377
Anti Erisalu <antikaz@gmail.com>
2023-12-04 00:49:15 +0200

    current, next and previous symbols with numbers

--- GearRatios/check/main.go
+++ GearRatios/check/main.go
@@ -1,3 +1,94 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"regexp"
+// 	"unicode"
+// )
+
+// type SymbolInfo struct {
+// 	Symbol rune
+// 	Index  int
+// }
+
+// type NumberInfo struct {
+// 	Number string
+// 	StartIndex  int
+// 	EndIndex	int
+// }
+
+// func getSymbolLocations(data string) []SymbolInfo {
+// 	var symbols []SymbolInfo
+
+// 	for i, char := range data {
+// 		if !unicode.IsDigit(char) && !unicode.IsLetter(char) && !unicode.IsSpace(char) && char != '.' {
+// 			symbols = append(symbols, SymbolInfo{Symbol: char, Index: i})
+// 		}
+// 	}
+
+// 	return symbols
+// }
+
+// func getNumberLocations(data string) []NumberInfo {
+// 	re := regexp.MustCompile(`\d+`)
+// 	matches := re.FindAllStringIndex(data, -1)
+// 	var numbers []NumberInfo
+
+// 	for _, match := range matches {
+// 		numbers = append(numbers, NumberInfo{
+// 			Number:     data[match[0]:match[1]],
+// 			StartIndex: match[0],
+// 			EndIndex:   match[1] - 1,
+// 		})
+// 	}
+
+// 	return numbers
+// }
+
+// func processFile() {
+// 	dataFile, err := os.Open("test.txt")
+// 	if err != nil {
+// 		log.Fatalf("Error opening file: %v", err)
+// 	}
+// 	defer dataFile.Close()
+
+// 	scanner := bufio.NewScanner(dataFile)
+// 	lineNumber := 1
+// 	var nextLineSymbols, previousLineSymbols, currentLineSymbols []SymbolInfo
+// 	var currData string
+
+// 	for scanner.Scan() {
+// 		nextLineSymbols = previousLineSymbols
+// 		previousLineSymbols = currentLineSymbols
+// 		currData = scanner.Text()
+// 		currentLineSymbols = getSymbolLocations(currData)
+// 		numbers := getNumberLocations(currData)
+
+// 		fmt.Printf("\nLine %d:\n", lineNumber)
+// 		for _, numberInfo := range numbers {
+// 			fmt.Printf("Number: %s, Location: %d-%d\n", numberInfo.Number, numberInfo.StartIndex, numberInfo.EndIndex)
+// 		}
+// 		for _, symbolInfo := range nextLineSymbols {
+// 			fmt.Printf("Previous line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+// 		}
+// 		for _, symbolInfo := range previousLineSymbols {
+// 			fmt.Printf("Current line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+// 		}
+// 		for _, symbolInfo := range currentLineSymbols {
+// 			fmt.Printf("Next line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+// 		}
+
+// 		lineNumber++
+// 	}
+// }
+
+// func main() {
+// 	processFile()
+// }
+
 package main
 
 import (
@@ -12,12 +103,6 @@ import (
 type SymbolInfo struct {
 	Symbol rune
 	Index  int
-}
-
-type NumberInfo struct {
-	Number string
-	StartIndex  int
-	EndIndex	int
 }
 
 func getSymbolLocations(data string) []SymbolInfo {
@@ -48,6 +133,12 @@ func getNumberLocations(data string) []NumberInfo {
 	return numbers
 }
 
+type NumberInfo struct {
+	Number     string
+	StartIndex int
+	EndIndex   int
+}
+
 func processFile() {
 	dataFile, err := os.Open("test.txt")
 	if err != nil {
@@ -57,30 +148,30 @@ func processFile() {
 
 	scanner := bufio.NewScanner(dataFile)
 	lineNumber := 1
-	var prevSymbols, currSymbols, nextSymbols []SymbolInfo
+	var nextLineSymbols, previousLineSymbols, currentLineSymbols []SymbolInfo
 	var currData string
 
 	for scanner.Scan() {
-		prevSymbols = currSymbols
-		currSymbols = nextSymbols
+		nextLineSymbols = previousLineSymbols
+		previousLineSymbols = currentLineSymbols
 		currData = scanner.Text()
-		nextSymbols = getSymbolLocations(currData)
+		currentLineSymbols = getSymbolLocations(currData)
 		numbers := getNumberLocations(currData)
-
-
 
 		fmt.Printf("\nLine %d:\n", lineNumber)
 		for _, numberInfo := range numbers {
 			fmt.Printf("Number: %s, Location: %d-%d\n", numberInfo.Number, numberInfo.StartIndex, numberInfo.EndIndex)
 		}
-		for _, symbolInfo := range prevSymbols {
+		for _, symbolInfo := range nextLineSymbols {
+			fmt.Printf("Next line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+		}
+		for _, symbolInfo := range previousLineSymbols {
 			fmt.Printf("Previous line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
 		}
-		for _, symbolInfo := range currSymbols {
-			fmt.Printf("Current line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
-		}
-		for _, symbolInfo := range nextSymbols {
-			fmt.Printf("Next line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+		if lineNumber != 1 {
+			for _, symbolInfo := range currentLineSymbols {
+				fmt.Printf("Current line Symbol: %c, Location: %d\n", symbolInfo.Symbol, symbolInfo.Index)
+			}
 		}
 
 		lineNumber++
